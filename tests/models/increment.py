7e2ee97c43ff09e1bebd571534ffe0722b0a0d94
bgawron9 <bartosz.gawron09@gmail.com>
2022-06-08 09:21:25 +0200

    increment for tf2

--- tests/models/increment.py
+++ tests/models/increment.py
@@ -15,43 +15,52 @@
 #
 
 import tensorflow as tf
+from tensorflow.python.framework import ops
 from tensorflow.python.saved_model import signature_constants
 from tensorflow.python.saved_model import tag_constants
 import numpy as np
+from keras import backend as K
 
 model_dir = "/tmp/increment/1"
-graph_def = tf.GraphDef()
 
-builder = tf.saved_model.builder.SavedModelBuilder(model_dir)
+@tf.function
+def f(x):
+  return x
+
+graph_def = f.get_concrete_function(1.).graph.as_graph_def()
+
+builder = tf.compat.v1.saved_model.builder.SavedModelBuilder(model_dir)
 sigs = {}
 
-with tf.Session(graph=tf.Graph()) as sess:
+
+
+
+with K.get_session() as sess:
     tf.import_graph_def(graph_def, name="")
-    input = tf.placeholder(tf.float32, [None, 10], name='input') # defines input shape
+    input = tf.keras.Input(name='input', shape=[10], dtype=tf.float32)  # defines input shape
     const = tf.constant(1.0, name="const")
     output = tf.add(input, const, name='output')  # defines network operation
 #    print(sess.graph.get_operations())
-    g = tf.get_default_graph()
+    g = ops.get_default_graph()
     inp = g.get_tensor_by_name("input:0")
     out = g.get_tensor_by_name("output:0")
 
     sigs[signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY] = \
-        tf.saved_model.signature_def_utils.predict_signature_def(
+        tf.compat.v1.saved_model.signature_def_utils.predict_signature_def(
             {"in": inp}, {"out": out})
 
     builder.add_meta_graph_and_variables(sess,[tag_constants.SERVING],signature_def_map=sigs)
 builder.save()
-
-with tf.Session(graph=tf.Graph()) as sess:
+tf.keras.backend.clear_session()
+with K.get_session() as sess:
     print("Loading model")
-    tf.saved_model.loader.load(sess, ["serve"], model_dir, clear_devices=True)
+    tf.compat.v1.saved_model.loader.load(sess, ["serve"], model_dir, clear_devices=True)
     print("Model loaded")
-    graph = tf.get_default_graph()
+    graph = ops.get_default_graph()
     input = graph.get_tensor_by_name("input:0")
     preds = graph.get_tensor_by_name("output:0")
-
     data = np.ones((1,10),np.float32)
     feed_dict = {input: data}
 
     results = sess.run([preds], feed_dict=feed_dict)
-    print(results)
+    print('results',results)
